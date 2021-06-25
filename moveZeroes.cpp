197abb845027c33791014cb3c6128b21cae07df0
Snofa <39087260+Snofa@users.noreply.github.com>
2021-06-25 21:31:50 +0200

    Create moveZeroes.cpp
    Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.

---
+++ moveZeroes.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Snofa on 25.06.2021.
+//
+
+#include <iostream>
+#include <vector>
+
+std::vector<int> move_zeroes(const std::vector<int>& input) {
+    std::vector<int> result = input;
+    for (int i = 0; i < result.size(); ++i) {
+        auto zero = std::find(begin(result), end(result), 0);
+        if (zero != result.end()){
+            result.erase(zero);
+            result.push_back(0);
+        }
+    }
+    return  result;
+
+}
